64a2f9f23b78225ba7cc86d8f801a12c78faced7
efran <efranhimel25@gmail.com>
2025-08-23 12:50:33 -0400

    applied lint

--- src/app.py
+++ src/app.py
@@ -6,18 +6,20 @@ import yaml
 if __name__ == "__main__":
     print("starting")
 
-    
-    with open("../config/spark_ml_config.yaml", 'r') as config_file:
+    with open("../config/spark_ml_config.yaml", "r") as config_file:
         config = yaml.safe_load(config_file)
 
     if "credit_card_fraud" in config and config["credit_card_fraud"]["enable"]:
         data_output_folder = utils.get_data(
             data_handle=config["credit_card_fraud"]["data_handle"],
-            data_folder=config["credit_card_fraud"]["data_folder"])
+            data_folder=config["credit_card_fraud"]["data_folder"],
+        )
 
         if data_output_folder:
             print("START")
-            spark_ml_pipeline.run(data_output_folder, **config["credit_card_fraud"]["job_config"])
+            spark_ml_pipeline.run(
+                data_output_folder, **config["credit_card_fraud"]["job_config"]
+            )
             print("FINISHED")
     else:
         print("skipping credit card fraud detection.")
--- src/services/utils.py
+++ src/services/utils.py
@@ -8,8 +8,10 @@ def get_data(data_handle: str, data_folder: str) -> str | None:
 
     data_output_folder = path.join("data", data_folder)
     if path.exists(data_output_folder) and listdir(data_output_folder):
-        print(f"Files in output path '{data_output_folder}':\n{listdir(data_output_folder)}\n"
-              f"Please clean if unintended.")
+        print(
+            f"Files in output path '{data_output_folder}':\n{listdir(data_output_folder)}\n"
+            f"Please clean if unintended."
+        )
         return None
     else:
         # TODO: for some reason kaggle download doesn't save to path so have to manually move
